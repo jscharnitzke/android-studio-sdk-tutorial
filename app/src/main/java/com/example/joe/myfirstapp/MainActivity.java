b146134f609ddb5c8778d733ee61f793f84e77ed
Joe Scharnitzke <j.scharnitzke@gmail.com>
2017-01-23 18:40:07 -0800

    Added an onClick behavior to the button and a stub method for the behavior

--- app/src/main/java/com/example/joe/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/joe/myfirstapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.joe.myfirstapp;
 
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        // Do stuff
     }
 }
