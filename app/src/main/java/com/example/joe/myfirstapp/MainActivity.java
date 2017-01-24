6084f85790fd8b0c143700098c279c29a76236df
Joe Scharnitzke <j.scharnitzke@gmail.com>
2017-01-23 18:44:28 -0800

    Added requirements for upcoming code changes

--- app/src/main/java/com/example/joe/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/joe/myfirstapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.joe.myfirstapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
